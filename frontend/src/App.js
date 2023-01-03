fd34742086bd77b1f917012f4d09049e69132e97
Arthur Alty <artouralty.jesus@gmail.com>
2023-01-03 19:03:28 +0300

    main page design created with react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
 import React, {useState, useEffect} from 'react';
+import {Button} from '@material-ui/core';
+
 import './App.css';
+
 import Post from './Post.js';
 
 
@@ -10,6 +12,8 @@ const BASE_URL = 'http://localhost:8000/'
 function App() {
 
   const [posts, setPosts] = useState([]);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [openSignUp, setOpenSignUp] = useState(false);
 
   useEffect(() => {
     fetch(BASE_URL + 'posts/')
@@ -22,6 +26,16 @@ function App() {
         throw response
       })
       .then(data => {
+        const result = data.sort((a, b) => {
+          const t_a = a.pub_date.split(/[-T:]/);
+          const t_b = b.pub_date.split(/[-T:]/);
+          const d_a = new Date(Date.UTC(t_a[0], t_a[1]-1, t_a[2], t_a[3], t_a[4], t_a[5]))
+          const d_b = new Date(Date.UTC(t_b[0], t_b[1]-1, t_b[2], t_b[3], t_b[4], t_b[5]))
+          return d_b - d_a
+        })
+        return result
+      })
+      .then(data => {
         setPosts(data)
       })
       .catch(error => {
@@ -30,14 +44,32 @@ function App() {
   }, [])
 
   return (
-    <div className='app_posts'>
-      {
-        posts.map(post => (
-          <Post
-           post = {post}
-          />
-        ))
-      }
+    <div className='app'>
+
+      <div className='app_header'>
+
+        <img className='app_headerLogo'
+          alt='InstagramClone Logo'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png'
+        />
+
+        <div>
+          <Button onClick={() => setOpenSignIn(true)}>Log In</Button>
+          <Button onClick={() => setOpenSignUp(true)}>Sign Up</Button>
+        </div>
+
+      </div>
+
+      <div className='app_posts'>
+        {
+          posts.map(post => (
+            <Post
+            post = {post}
+            />
+          ))
+        }
+      </div>
+
     </div>
   );
 }
--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import {Avatar, Button} from '@material-ui/core';
+
 import './Post.css'
 
 
@@ -8,17 +10,53 @@ const BASE_URL = 'http://localhost:8000/'
 function Post({ post }) {
 
   const [imageURL, setImageURL] = useState('')
+  const [comments, setComments] = useState([])
 
   useEffect(() => {
     setImageURL(BASE_URL + post.image_url)
   }, [])
 
+  useEffect(() => {
+    setComments(post.comments)
+  }, [])
+
   return (
     <div className='post'>
+
+      <div className='post_header'>
+        <Avatar
+          alt='user-avatar'
+          src=''
+        />
+        <div className='post_headerInfo'>
+          <h3>{post.owner.username}</h3>
+          <Button className='post_delete'>Delete</Button>
+        </div>
+        {/* <div className='post_headerInfo'>
+          <h3>{post.author.username}</h3>
+          <Button className='post_delete'>Delete</Button>
+        </div> */}
+      </div>
+
       <img
         className='post_image'
         src={imageURL}
       />
+
+      <h4 className='post_caption'>{post.caption}</h4>
+
+      <div className='post_comments'>
+        {
+          comments.map((comment) => (
+            <p>
+              <strong>
+                {comment.author.username}:
+              </strong>
+              {comment.text}
+            </p>
+          ))
+        }
+      </div>
     </div>
   )
 }
